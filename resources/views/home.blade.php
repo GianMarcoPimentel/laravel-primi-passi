d9ed4f703ccb9c0fc464fed1ab8e144e06a2ebe4
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-05 16:52:55 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><laravel-primi-passo></laravel-primi-passo></title>
+</head>
+<body>
+    
+</body>
+</html>
