4190c23aefca2479e79f8bd0840fa52395b9a2a3
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-05 17:00:08 +0200

    Fine esercizio

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    //creo uan variabile
+
+    $saluto = 'Hello World';
+
+    return view('home', compact('saluto'));
 });
